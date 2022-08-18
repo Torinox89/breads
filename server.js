9311895d37e4514908d074dd1ce90eceef028918
Adrian Mitre <mitrehealthcare@gmail.com>
2022-08-17 19:49:12 -0700

    BreadCRUD helper methods part 1,2 anD validation

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-
-    mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
     )
 
